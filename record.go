ab478a3d6b877349f1817554fe1a73aa14ffd270
Julien Cuvillier <julien.cuvillier@mycs.com>
2020-12-04 10:08:38 +0100

    Add print functions

--- record.go
+++ record.go
@@ -1,10 +1,40 @@
 package csvhandler
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 // Record holds the fields for a given entry.
-// It offers utility functions to access field based on the column name.
+// It offers utility functions to access field based on the column name
 type Record struct {
 	headers map[string]int
 	values  []string
+}
+
+// Fprintln prints into the given writer each given column with a 'key=value' format.
+// For instance, Fprintln(w, "first_name", "last_name") writes "first_name='John' last_name='Smith'"
+// Expected errors are the same Get() may return
+func (r Record) Fprintln(w io.Writer, columns ...string) error {
+	for _, c := range columns {
+		v, err := r.Get(c)
+		if err != nil {
+			return err
+		}
+		if _, err := fmt.Fprintf(w, "%s='%s' ", c, v); err != nil {
+			return err
+		}
+	}
+	_, err := fmt.Fprint(w, "\n")
+	return err
+}
+
+// Println prints into the standard output each given column with a 'key=value' format.
+// For instance, Fprintln(w, "first_name", "last_name") writes "first_name='John' last_name='Smith'"
+// Expected errors are the same Get() may return
+func (r Record) Println(columns ...string) error {
+	return r.Fprintln(os.Stdout, columns...)
 }
 
 // Get returns as a string the field corresponding to the given key.
